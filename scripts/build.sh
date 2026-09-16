251f90a7bdee10d0d8884d9b0872febe45140d38
sunzhiyouvv-tech <sunzhiyouvv@gmail.com>
2026-09-16 11:43:27 +0800

    Add ITJK Markdown build script

---
+++ scripts/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+ROOT=/tmp/doocs-md
+rm -rf $ROOT
+
+git clone --depth 1 https://github.com/doocs/md.git $ROOT
+cd $ROOT
+
+# ITJK branding replacements
+find . -type f \( -name '*.js' -o -name '*.ts' -o -name '*.vue' -o -name '*.json' -o -name '*.html' -o -name '*.css' \) -print0 | xargs -0 sed -i \
+  -e 's/doocs/ITJK/g' \
+  -e 's/Doocs/ITJK/g' \
+  -e 's/DOOCS/ITJK/g'
+
+npm install
+npm run build
+
+cp -r dist $OLDPWD/dist
